9f9df0e7423bb46670c8ddd21a178ec49f3376d9
Sebastian <sebastiangeido@gmail.com>
2022-09-14 23:32:44 -0300

    Fetch

--- js/main.js
+++ js/main.js
@@ -19,15 +19,30 @@ function sinCamiseta () {
         padding: '3em',
         color: '#143F6B',
         background: `url(https://i.pinimg.com/736x/80/05/36/800536363d097be1244d85726d754f39.jpg)`,
-        backdrop: `
-          rgba(0,0,123,0.4)
-          url("/images/nyan-cat.gif")
-          left top
-          no-repeat
-        `
     })
 }
-class Camiseta {
+fetch('productos.json')
+    .then((res) => res.json())
+    .then((json) => {
+        let html = ' ';
+        json.forEach((camiseta) => {
+/*             let textoCamiseta = document.getElementsByClassName("containerArticulo")
+            textoCamiseta.innerHTML = `<p>${camiseta.Nombre} <br /> ${camiseta.Precio}</p>` */
+            html += `<div class="containerArticulo">
+                        <a onclick="sinCamiseta()">
+                            <img class="IMGcamiseta"
+                            src="${camiseta.link}"
+                            alt="Camiseta Boca Juniors Titular 2022"
+                            />
+                            ${camiseta.Nombre} <br /> $${camiseta.Precio}
+                        </a>
+                    </div>`;
+        }); 
+        document.getElementById('catalogo').innerHTML = html;
+        
+    })
+
+/* class Camiseta {
     constructor(nombre, precio, seleccionado) {
         this.nombre = nombre
         this.precio = precio
@@ -41,7 +56,7 @@ camisetas.push(new Camiseta("BocaAlt22", 12000, false))
 camisetas.push(new Camiseta("RivTit22", 10000, true))
 camisetas.push(new Camiseta("RivSup22", 12000, false))
 camisetas.push(new Camiseta("RivArq22", 14000, false))
-camisetas.push(new Camiseta("RivAlt22", 15000, false))
+camisetas.push(new Camiseta("RivAlt22", 15000, false)) */
 function contarSeleccionados(_seleccionados) {
     if (_seleccionados == true)
         this.itemsSeleccionados = itemsSeleccionados + 1
@@ -57,23 +72,23 @@ function aplicarDescuento(precio, descuento, nombre) {
     this.precioConDescuento = precio - precio * (descuento / 100);
     console.log(`El precio final de ${nombre} es de ${precioConDescuento}`)
 }
-for (const Camiseta of camisetas) {
+/* for (const Camiseta of camisetas) {
     aplicarDescuento(Camiseta["precio"], 30, Camiseta["nombre"])
-}
-for (const Camiseta of camisetas) {
+} */
+/* for (const Camiseta of camisetas) {
     contarSeleccionados(Camiseta["seleccionado"])
 }
-contarItems()
-let camisetasConIva = camisetas.map((el) => {
+contarItems() */
+/* let camisetasConIva = camisetas.map((el) => {
     return {
         nombre: el.nombre,
         precio: el.precio * 1.21,
         seleccionado: el.seleccionado
     }
-})
+}) */
 /* let titulo1 = document.getElementById("mainTitle");
 titulo1.innerHTML = ; */
-camisetasConIva.sort((a, b) => {
+/* camisetasConIva.sort((a, b) => {
     if (a.precio > b.precio) {
         return 1;
     }
@@ -82,7 +97,7 @@ camisetasConIva.sort((a, b) => {
     }
 })
 console.log(camisetasConIva)
-
+ */
 function darkMode() {
     let el = document.body;
     el.classList.toggle(`darkMode`)
