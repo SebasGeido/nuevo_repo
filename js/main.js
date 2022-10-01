9acbe684df4004a8955eb7dcb7b8f760814716b2
Sebastian <sebastiangeido@gmail.com>
2022-10-01 11:59:20 -0300

    Proyecto Final (JS)

--- js/main.js
+++ js/main.js
@@ -26,78 +26,30 @@ fetch('productos.json')
     .then((json) => {
         let html = ' ';
         json.forEach((camiseta) => {
-/*             let textoCamiseta = document.getElementsByClassName("containerArticulo")
-            textoCamiseta.innerHTML = `<p>${camiseta.Nombre} <br /> ${camiseta.Precio}</p>` */
-            html += `<div class="containerArticulo">
-                        <a onclick="sinCamiseta()">
-                            <img class="IMGcamiseta"
-                            src="${camiseta.link}"
-                            alt="Camiseta Boca Juniors Titular 2022"
-                            />
-                            ${camiseta.Nombre} <br /> $${camiseta.Precio}
-                        </a>
-                    </div>`;
+            if(camiseta.Stock == true){
+                html += `<div class="containerArticulo">
+                            <a href="">
+                                <img class="IMGcamiseta"
+                                src="${camiseta.link}"
+                                alt="Camiseta Boca Juniors Titular 2022"
+                                />
+                                ${camiseta.Nombre} <br /> $${camiseta.Precio}
+                            </a>
+                        </div>`;
+            }else {
+                html += `<div class="containerArticulo">
+                <a onclick="sinCamiseta()">
+                    <img class="IMGcamiseta"
+                    src="${camiseta.link}"
+                    alt="Camiseta Boca Juniors Titular 2022"
+                    />
+                    ${camiseta.Nombre} <br /> $${camiseta.Precio}
+                </a>
+            </div>`;
+            }
         }); 
         document.getElementById('catalogo').innerHTML = html;
-        
     })
-
-/* class Camiseta {
-    constructor(nombre, precio, seleccionado) {
-        this.nombre = nombre
-        this.precio = precio
-        this.seleccionado = seleccionado
-    }
-}
-const camisetas = [];
-camisetas.push(new Camiseta(`BocaTit22`, 10000, true))
-camisetas.push(new Camiseta("BocaSup22", 12000, false))
-camisetas.push(new Camiseta("BocaAlt22", 12000, false))
-camisetas.push(new Camiseta("RivTit22", 10000, true))
-camisetas.push(new Camiseta("RivSup22", 12000, false))
-camisetas.push(new Camiseta("RivArq22", 14000, false))
-camisetas.push(new Camiseta("RivAlt22", 15000, false)) */
-function contarSeleccionados(_seleccionados) {
-    if (_seleccionados == true)
-        this.itemsSeleccionados = itemsSeleccionados + 1
-}
-function contarItems() {
-    if (this.itemsSeleccionados === 0)
-        console.log(`Su carrito esta vacío`)
-    else {
-        console.log(`Tiene ${this.itemsSeleccionados} articulos seleccionados`)
-    }
-}
-function aplicarDescuento(precio, descuento, nombre) {
-    this.precioConDescuento = precio - precio * (descuento / 100);
-    console.log(`El precio final de ${nombre} es de ${precioConDescuento}`)
-}
-/* for (const Camiseta of camisetas) {
-    aplicarDescuento(Camiseta["precio"], 30, Camiseta["nombre"])
-} */
-/* for (const Camiseta of camisetas) {
-    contarSeleccionados(Camiseta["seleccionado"])
-}
-contarItems() */
-/* let camisetasConIva = camisetas.map((el) => {
-    return {
-        nombre: el.nombre,
-        precio: el.precio * 1.21,
-        seleccionado: el.seleccionado
-    }
-}) */
-/* let titulo1 = document.getElementById("mainTitle");
-titulo1.innerHTML = ; */
-/* camisetasConIva.sort((a, b) => {
-    if (a.precio > b.precio) {
-        return 1;
-    }
-    if (a.precio < b.precio) {
-        return -1;
-    }
-})
-console.log(camisetasConIva)
- */
 function darkMode() {
     let el = document.body;
     el.classList.toggle(`darkMode`)
@@ -106,7 +58,7 @@ function darkMode() {
 function lightMode() {
     let el = document.body;
     el.classList.toggle(`darkMode`)
-    localStorage.clear()
+    localStorage.removeItem(`color`)
 }
 let bodyColor = localStorage.getItem(`color`)
 
